6ecf468d3ec588632755fe01ff4b1ee5dceeaae1
earceg205 <earceg205@ulacit.ed.cr>
2024-08-06 20:22:35 -0600

    Cambios perfil - editar perfil
    Limpie mucho codigo para dejar solo lo esencial que vamos a utilizar

--- src/views/dashboard/app/user-profile-edit.jsx
+++ src/views/dashboard/app/user-profile-edit.jsx
@@ -27,11 +27,7 @@ const UserProfileEdit = () => {
                                                         Change Password
                                                     </Nav.Link>
                                                 </Nav.Item>
-                                                <Nav.Item as="li" className="col-md-3 p-0">
-                                                    <Nav.Link eventKey="third" role="button">
-                                                        Email and SMS
-                                                    </Nav.Link>
-                                                </Nav.Item>
+                            
                                                 <Nav.Item as="li" className="col-md-3 p-0">
                                                     <Nav.Link eventKey="fourth" role="button">
                                                         Manage Contact
@@ -178,73 +174,7 @@ const UserProfileEdit = () => {
                                             </Card.Body>
                                         </Card>
                                     </Tab.Pane>
-                                    <Tab.Pane eventKey="third" className="fade show">
-                                        <Card>
-                                            <Card.Header className="card-header d-flex justify-content-between">
-                                                <div className="header-title">
-                                                    <h4 className="card-title">Email and SMS</h4>
-                                                </div>
-                                            </Card.Header>
-                                            <Card.Body>
-                                                <Form>
-                                                    <Form.Group className="form-group row align-items-center">
-                                                        <div className="col-md-3" htmlFor="emailnotification">Email Notification:</div>
-                                                        <Form.Check className="col-md-9 form-check form-switch">
-                                                            <Form.Check.Input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked11" defaultChecked />
-                                                            <Form.Check.Label className="form-check-label" htmlFor="flexSwitchCheckChecked11">Checked switch checkbox input</Form.Check.Label>
-                                                        </Form.Check>
-                                                    </Form.Group>
-                                                    <Form.Group className="form-group row align-items-center">
-                                                        <div className="col-md-3" htmlFor="smsnotification">SMS Notification:</div>
-                                                        <Form.Check className="col-md-9 form-check form-switch">
-                                                            <Form.Check.Input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked12" defaultChecked />
-                                                            <Form.Check.Label className="form-check-label" htmlFor="flexSwitchCheckChecked12">Checked switch checkbox input</Form.Check.Label>
-                                                        </Form.Check>
-                                                    </Form.Group>
-                                                    <Form.Group className="form-group row align-items-center">
-                                                        <div className="col-md-3" htmlFor="npass">When To Email</div>
-                                                        <Col md="9">
-                                                            <Form.Check className="form-check">
-                                                                <Form.Check.Input className="form-check-input" type="checkbox" defaultValue="" id="flexCheckDefault12" />
-                                                                <Form.Check.Label className="form-check-label" htmlFor="flexCheckDefault12">
-                                                                    You have new notifications.
-                                                                </Form.Check.Label>
-                                                            </Form.Check>
-                                                            <Form.Check className="form-check d-block">
-                                                                <Form.Check.Input className="form-check-input" type="checkbox" defaultValue="" id="email02" />
-                                                                <Form.Check.Label className="form-check-label" htmlFor="email02">You're sent a direct message</Form.Check.Label>
-                                                            </Form.Check>
-                                                            <Form.Check className="form-check d-block">
-                                                                <Form.Check.Input type="checkbox" className="form-check-input" id="email03" defaultChecked />
-                                                                <Form.Check.Label className="form-check-label" htmlFor="email03">Someone adds you as a connection</Form.Check.Label>
-                                                            </Form.Check>
-                                                        </Col>
-                                                    </Form.Group>
-                                                    <Form.Group className="form-group row align-items-center">
-                                                        <div className="col-md-3" htmlFor="npass">When To Escalate Emails</div>
-                                                        <Col md="9">
-                                                            <Form.Check className="form-check">
-                                                                <Form.Check.Input className="form-check-input" type="checkbox" defaultValue="" id="email04" />
-                                                                <Form.Check.Label className="form-check-label" htmlFor="email04">
-                                                                    Upon new order.
-                                                                </Form.Check.Label>
-                                                            </Form.Check>
-                                                            <Form.Check className="form-check d-block">
-                                                                <Form.Check.Input className="form-check-input" type="checkbox" defaultValue="" id="email05" />
-                                                                <Form.Check.Label className="form-check-label" htmlFor="email05">New membership approval</Form.Check.Label>
-                                                            </Form.Check>
-                                                            <Form.Check className="form-check d-block">
-                                                                <Form.Check.Input type="checkbox" className="form-check-input" id="email06" defaultChecked />
-                                                                <Form.Check.Label className="form-check-label" htmlFor="email06">Member registration</Form.Check.Label>
-                                                            </Form.Check>
-                                                        </Col>
-                                                    </Form.Group>
-                                                    <Button type="submit" className="btn btn-primary me-2">Submit</Button>{" "}
-                                                    <button type="reset" className="btn btn-danger-subtle">Cancel</button>
-                                                </Form>
-                                            </Card.Body>
-                                        </Card>
-                                    </Tab.Pane>
+                                 
                                     <Tab.Pane eventKey="fourth" className="fade show">
                                         <Card>
                                             <Card.Header className="d-flex justify-content-between">
