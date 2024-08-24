98028b25e8864683e9f4381e98109ae36145dfca
Jose David Arce González <jarceg884@ulacit.ed.cr>
2024-08-24 12:18:17 -0600

    Update Admin

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -8,7 +8,7 @@ import Card from "../../components/Card";
 import CreatePost from "../../components/create-post";
 import Post from "../../components/Post";
 import Swal from "sweetalert2"; // Importar SweetAlert2
-
+ 
 // Imágenes
 import user14 from "../../assets/images/user/06.jpg";
 import user15 from "../../assets/images/user/07.jpg";
@@ -20,19 +20,19 @@ import busImg from "../../assets/images/page-img/bus.webp";
 import img11 from "../../assets/images/page-img/fd.webp";
 import mountain from "../../assets/images/page-img/mountain.webp";
 import pizza from "../../assets/images/page-img/pizza.webp";
-
+ 
 // FsLightbox
 import ReactFsLightbox from "fslightbox-react";
-
+ 
 const FsLightbox = ReactFsLightbox.default
   ? ReactFsLightbox.default
   : ReactFsLightbox;
-
+ 
 const SuggestionsList = () => {
   const [users, setUsers] = useState([]);
   const auth = getAuth();
   const currentUser = auth.currentUser;
-
+ 
   useEffect(() => {
     const usersCollection = collection(db, "users");
     const unsubscribe = onSnapshot(usersCollection, (snapshot) => {
@@ -42,17 +42,17 @@ const SuggestionsList = () => {
           ...doc.data(),
         }))
         .filter((user) => user.uid !== currentUser?.uid && user.role !== "admin"); // Excluir al usuario actual y a los administradores
-
+ 
       const randomUsers = usersList
         .sort(() => 0.5 - Math.random())
         .slice(0, 3);
-
+ 
       setUsers(randomUsers);
     });
-
+ 
     return () => unsubscribe();
   }, [currentUser]);
-
+ 
   // Función para verificar si ya se ha enviado una solicitud de seguimiento
   const checkIfFollowRequestExists = async (fromUid, toUid) => {
     const followRequestsCollection = collection(db, "followRequests");
@@ -62,11 +62,11 @@ const SuggestionsList = () => {
       where("to", "==", toUid),
       where("status", "==", "pending")
     );
-
+ 
     const querySnapshot = await getDocs(q);
     return !querySnapshot.empty; // Si hay algún resultado, significa que ya existe una solicitud
   };
-
+ 
   // Función para enviar solicitud de seguimiento
   const handleSendFollowRequest = async (userUid) => {
     if (!currentUser) {
@@ -78,7 +78,7 @@ const SuggestionsList = () => {
       });
       return;
     }
-
+ 
     // Verificar si el usuario está intentando enviarse una solicitud a sí mismo
     if (currentUser.uid === userUid) {
       Swal.fire({
@@ -89,7 +89,7 @@ const SuggestionsList = () => {
       });
       return;
     }
-
+ 
     // Verificar si ya existe una solicitud de seguimiento
     const requestExists = await checkIfFollowRequestExists(currentUser.uid, userUid);
     if (requestExists) {
@@ -101,7 +101,7 @@ const SuggestionsList = () => {
       });
       return;
     }
-
+ 
     try {
       // Enviar la solicitud de seguimiento
       await addDoc(collection(db, "followRequests"), {
@@ -126,12 +126,12 @@ const SuggestionsList = () => {
       });
     }
   };
-
+ 
   // Función para eliminar el usuario de la lista
   const handleRemoveUser = (userUid) => {
     setUsers((prevUsers) => prevUsers.filter((user) => user.uid !== userUid));
   };
-
+ 
   return (
     <Card>
       <div className="card-header d-flex justify-content-between">
@@ -183,7 +183,76 @@ const SuggestionsList = () => {
     </Card>
   );
 };
-
+ 
+const AdminSuggestionsList = () => {
+  const [users, setUsers] = useState([]);
+ 
+  useEffect(() => {
+    const usersCollection = collection(db, "users");
+    const unsubscribe = onSnapshot(usersCollection, (snapshot) => {
+      const usersList = snapshot.docs
+        .map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        .filter((user) => user.role === "admin");
+ 
+      const randomUsers = usersList
+        .sort(() => 0.5 - Math.random())
+        .slice(0, 3);
+ 
+      setUsers(randomUsers);
+    });
+ 
+    return () => unsubscribe();
+  }, []);
+ 
+  return (
+    <Card>
+      <div className="card-header d-flex justify-content-between">
+        <div className="header-title">
+          <h4 className="card-title">Admin Suggestions</h4>
+        </div>
+      </div>
+      <Card.Body className="pt-0">
+        <ul className="list-inline m-0 p-0">
+          {users.map((user) => (
+            <li className="mb-3" key={user.uid}>
+              <div className="d-flex align-items-center gap-2 justify-content-between">
+                <div className="d-flex align-items-center gap-3">
+                  <img
+                    src={user.profilePic || "https://via.placeholder.com/60"}
+                    alt="profile"
+                    className="avatar-60 avatar-borderd object-cover avatar-rounded img-fluid d-inline-block"
+                  />
+                  <div>
+                    <h5>{user.username}</h5>
+                    <small className="text-capitalize">
+                      Admin role user
+                    </small>
+                    <br />
+                    <small className="text-muted">
+                      <FaMapMarkerAlt /> {user.country}
+                    </small>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center flex-shrink-0 gap-2">
+                  <button className="btn btn-primary-subtle p-1 lh-1">
+                    <FaPlus className="font-size-14" />
+                  </button>
+                  <button className="btn btn-danger-subtle p-1 lh-1">
+                    <FaTimes className="font-size-14" />
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Card.Body>
+    </Card>
+  );
+};
+ 
 const Index = () => {
   const [posts, setPosts] = useState([]);
   const [loadContent, setLoadContent] = useState(true);
@@ -193,21 +262,21 @@ const Index = () => {
   });
   const [isReportChecked, setIsReportChecked] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
-
+ 
   useEffect(() => {
     const auth = getAuth();
     const user = auth.currentUser;
-
+ 
     if (user && user.uid === "5C2Fj2rimGQeQK9HxG4Z7VDjlJl2") {
       setIsAdmin(true);
     }
   }, []);
-
+ 
   // Obtener los posts en tiempo real desde Firebase, ordenados por createdAt
   useEffect(() => {
     const postsCollection = collection(db, "posts");
     const postsQuery = query(postsCollection, orderBy("createdAt", "desc"));
-
+ 
     const unsubscribe = onSnapshot(postsQuery, (snapshot) => {
       const postsList = snapshot.docs.map((doc) => ({
         id: doc.id,
@@ -215,10 +284,10 @@ const Index = () => {
       }));
       setPosts(postsList);
     });
-
+ 
     return () => unsubscribe();
   }, []);
-
+ 
   useEffect(() => {
     function handleScroll() {
       if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
@@ -227,32 +296,32 @@ const Index = () => {
         }, 2000);
       }
     }
-
+ 
     window.addEventListener("scroll", handleScroll);
-
+ 
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
+ 
   function imageOnSlide(number) {
     setImageController({
       toggler: !imageController.toggler,
       slide: number,
     });
   }
-
+ 
   // Manejador de clics para los posts
   const handlePostClick = (postId) => {
     console.log("Post ID:", postId);
   };
-
+ 
   const handleCheckboxChange = () => {
     setIsReportChecked(!isReportChecked);
   };
-
+ 
   const postFiltered = posts.filter((post) => post.reported === true);
-
+ 
   return (
     <>
       <div id="content-page" className="content-inner">
@@ -270,7 +339,7 @@ const Index = () => {
                     <CreatePost className="card-block card-stretch card-height" />
                   </Col>
                 </Row>
-
+ 
                 {isAdmin && (
                   <div className="mb-3">
                     <label>
@@ -283,7 +352,7 @@ const Index = () => {
                     </label>
                   </div>
                 )}
-
+ 
                 <Row className="special-post-container">
                   {isReportChecked
                     ? postFiltered.map((post) => (
@@ -316,7 +385,7 @@ const Index = () => {
                           onPostClick={handlePostClick}
                         />
                       ))}
-
+ 
                   {loadContent && (
                     <div className="col-sm-12 text-center">
                       <img
@@ -329,10 +398,10 @@ const Index = () => {
                 </Row>
               </div>
             </Col>
-
+ 
             <Col lg={4}>
               <div className="fixed-suggestion mb-0 mb-lg-4">
-                <SuggestionsList />
+                {isAdmin ? <AdminSuggestionsList /> : <SuggestionsList />}
               </div>
             </Col>
           </Row>
@@ -341,5 +410,5 @@ const Index = () => {
     </>
   );
 };
-
-export default Index;
+ 
+export default Index
